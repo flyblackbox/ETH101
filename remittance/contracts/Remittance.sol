4e58eb6ad1fd4c2d7c71c353686eabd7d960c309
flyblackbox <louis@flyblackbox.com>
2017-08-30 20:40:33 -0400

    Update Remittance.sol
    Round 2 Revisions

--- remittance/contracts/Remittance.sol
+++ remittance/contracts/Remittance.sol
@@ -1,155 +1,131 @@
 pragma solidity ^0.4.6;
 
-contract Owner() {
-  function Owner() {
-      owner = tx.origin;
-  }
-}
-
-contract Remitance(uint fee) {
-
-    address owner;
+contract Remitance is Owner {
     uint fee;
     uint feeBalance;
     bytes32 keyHash;
 
-//Establish the owner, the fee, and the starting balance
-    function Remittance(uint feeAmount) {
-        owner = Owner.owner;
-        fee = feeAmount;
+    //Establish the owner, the fee, and the starting balance
+    function Remittance() {
         feeBalance = 0;
     }
 
-//Create a struct to keep track of participants and critical details
-    struct TransactionStruct {
-        address origin; // Alice
-        address destination; // Carol
-        uint amount;
-        uint deadlineBlock;
-        //bool hasDeadline; Assume a deadline
-    }
-
-    mapping(bytes32 => TransactionStruct) remittanceBook;
-
-    event LogSetFees(uint newFee);
-    event LogWithdrawFees(uint balance);
-    event LogSend(address origin, address destination, uint deadlineBlock, bytes32 keyHash);
-    event LogCollect(bytes32 keyHash);
-    event LogRefund(bytes32 keyHash);
-    event LogKillSwitch();
-
-//Restrict functions to the contract owner
-    modifier restricted() {
+    //Restrict some functions to the contract owner
+    modifier isCarol() {
         if (msg.sender == owner) _;
     }
 
-    function send(address destination, uint deadline)
-        public
-        payable
-        returns(bool) {
-    //Require the transaction to have a value, greater than the fee set.
-            require(msg.value > fee);
-
-            require(!remittanceBook[keyHash].amount = 0);
-            remittanceBook[keyHash] = TransactionStruct({
-                                        origin: msg.sender,
-                                        destination: destination,
-                                        amount: msg.value - fee,
-                                        deadlineBlock: block.number + deadline,
-                    //Deadline assumed  hasDeadline: deadline > 0,
-                                        });
-
-            feeBalance += fee;
-            LogSend(msg.sender, dest, deadline, keyHash);
-
-            return true;
+    //Create a struct to keep track of participants and critical details
+    struct TransactionStruct {
+        address origin;     //Who sent Carol the money?
+        uint amount;        //How much was sent?
+        unit fee;           //What was the fee at the time the transfer was sent?
+        uint deadlineBlock; //When did the transfer have to be withdrawn by?
     }
 
-//Set the fee upon deployment, or change while live via public function
+    //Keep track of all remittance in a map
+    mapping(bytes32 => TransactionStruct) remittanceBook;
+
+    event LogSetFees(uint newFee);
+    event LogSend(address origin, address destination, uint deadlineBlock);
+    event LogCollection(bytes32 keyHash);
+    event LogFeeWithraw(uint withrawAmount);
+    event LogRefund(bytes32 keyHash);
+    event LogKillSwitch();
+
+    //Carol must set a fee after deployment, no fee is set by default. Can be changed.
     function setFee(uint newFee)
         public
-        restricted
+        isCarol()
         returns(bool) {
         fee = newFee;
-        LogSetFees(newFee, "A new fee is set");
+        LogSetFees(newFee);
         return true;
     }
 
-//Receive two passwords as inputs, hash them, and then has their hashes to create a keyHash
-    function setPasswords(bytes32 firstPassword, bytes32 secondPassword) {
+    //Receive two passwords as inputs, hash them, and then has their hashes to create a keyHash
+    function setSecureKeyHash(bytes32 firstPassword, bytes32 secondPassword) {
         keyHash = keccak256(firstPassword, secondPassword);
     }
 
+    //Send ETH to Carol to convert for Bob.
+    function sendToCarol(uint deadline, bytes32 firstPassword, bytes32 secondPassword)
+        public
+        payable{
 
-//The public can see how much the owner has left in the contract to be collected
+            setSecureKeyHash(firstPassword, secondPassword);
+            //Require the transaction to have a value, greater than the fee set.
+            require(msg.value > fee);
+            //Only allow one transfer at a time
+            require(remittanceBook[keyHash].amount = 0); //Otherwise, new password must be set.
+            remittanceBook[keyHash] = TransactionStruct({
+                                        origin: msg.sender,
+                                        amount: msg.value - fee,
+                                        fee: fee,
+                                        deadlineBlock: block.number + deadline,
+                                        });
+
+            feeBalance += fee;
+            LogSend(msg.sender, deadline, keyHash);
+    }
+
+
+    //Carol can see how many fees she has collected
     function checkAccumulatedFees()
         public
         constant
+        isCarol()
         returns(uint){
         return feeBalance;
     }
 }
 
-//Only the owner can withdraw the fees collected
+    //Only Carol can withdraw the fees collected
     function withdrawAccumulatedFees()
         public
-        restricted
-        returns(bool) {
-		uint amount = feeBalance;
-
-//If this is not the owner, revert. Otherwise send, reset the fee balance and log.
-  		if(!owner.send(amount)) revert();
-
-      feeBalance = 0;
-      LogWithdrawFees(amount);
-      return true;
+        isCarol(){
+          owner.transfer(feeBalance);
+          LogFeeWithdraw(feeBalance);
+          feeBalance = 0;
     }
 
     function checkRemitanceBalance(bytes32 firstPassword, bytes32 secondPassword)
+        public
         constant
         returns(uint){
-            keyHash = keccak256(firstPassword, secondPassword);
+            setSecureKeyHash(firstPassword, secondPassword);
             return remittanceBook[keyHash].amount;
     }
 
     function collect(bytes32 firstPassword, bytes32 secondPassword)
         public
-        returns(bool){
+        isCarol(){
+        setSecureKeyHash(firstPassword, secondPassword);
 
-        keyHash = keccak256(firstPassword, secondPassword);
+        require(remittanceBook[keyHash].amount > 0);
 
-        require(remittanceBook[keyHash].destination == msg.sender &&
-                remittanceBook[keyHash].amount > 0 &&
-                );
-
+        carol.send(remittanceBook[keyHash].amount);
         remittanceBook[keyHash].amount = 0;
-    //if(!msg.sender.send(remitanceBook[keyHash].amount)) revert(); prefer transfer below?
-    msg.sender.transfer(remittanceBook[keyHash].amount);
-
-    LogCollect(keyHash);
-    return true;
-
-//Refund owner if ether isn't claimed
-    function refund()
-        returns(bool){
-
-//Require
-        require(remittanceBook[keyHash].origin == msg.sender &&
-                remittanceBook[keyHash].amount > 0 &&
-                );
-
-      //Deadline is assumed  if(remittanceBook[keyHash].hasDeadline){}
-        require(block.number > remittanceBook[keyHash].deadlineBlock);
-		remittanceBook[keyHash].amount = 0;
-//prefer transfer below? Instead of: if(!msg.sender.send(remitanceBook[keyHash].amount)) revert();
-		msg.sender.transfer(remittanceBook[keyHash].amount);
-		LogRefund(keyHash);
-		return true;
+        LogCollection(keyHash);
     }
 
+    //Refund owner if ether isn't claimed before deadline
+    function refund(bytes32 firstPassword, bytes32 secondPassword)
+        public{
+        setSecureKeyHash(firstPassword, secondPassword);
+
+        require(remittanceBook[keyHash].amount > 0);
+        require(remittanceBook[keyHash].origin == msg.sender;
+        require(block.number > remittanceBook[keyHash].deadlineBlock);
+        msg.sender.transfer(remittanceBook[keyHash].amount);
+        remittanceBook[keyHash].amount = 0;
+        LogRefund(keyHash);
+    }
+
+    //Only the owner can terminate the contract
     function killSwitch()
         public
-        restricted {
+        isOwner {
         LogKillSwitch();
         selfdestruct(owner);
     }

08de3b50b5586bbc61f33e47cfc07597f8083cd2
flyblackbox <louis@flyblackbox.com>
2017-09-09 09:46:30 -0400

    Add files via upload
    Clean update

---
+++ splitter/Splitter.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.4;
+
+
+contract Splitter {
+
+    address public owner;
+    mapping(address => uint) balance;
+
+//Establish the owner of the contract upon instantiation
+    function Splitter() {
+        owner = msg.sender;
+    }
+
+//Restrict functions to be used by the owner only
+    modifier isOwner() {
+        if (msg.sender == owner) _;
+    }
+
+//Setup event logs
+    event LogSplit(address sender, address receiver1, address receiver2, uint amount);
+    event LogWithdrawn(address to, bool success);
+    event LogKillSwitch();
+
+//Create a payable function that splits value sent between two different accounts
+    function split(address receiver1, address receiver2)
+        public
+        payable
+        returns(bool) {
+
+//Require value being sent to the function, and the existence of two addresses input by the owner
+        require(msg.value > 0);
+        require(receiver1 != 0 && receiver2 !=0);
+
+//Split the value in half, and distribute it to each address
+        balance[receiver1] += msg.value / 2;
+        balance[receiver2] += msg.value / 2;
+
+//Return the remainder to the owner
+        if(msg.value % 2 == 1){
+            balance[owner] += 1;
+        }
+
+        LogSplit(msg.sender, receiver1, receiver2, msg.value);
+
+        return true;
+    }
+
+//Let the public find the balance of the contract
+    function getBalance(address rec)
+        public
+        constant
+        returns(uint) {
+        return balance[rec];
+    }
+
+
+
+    function withdraw()
+        public
+        returns(bool) {
+
+//Check the balance of the person trying to withdraw
+        uint amount = balance[msg.sender];
+
+        require(amount > 0);
+
+//should this balance be set to 0 before or after the transfer?
+        balance[msg.sender] = 0;
+        msg.sender.transfer(amount);
+        LogWithdrawn(msg.sender, true);
+
+      return true;
+
+    }
+
+
+    function killSwitch()
+        public
+        isOwner {
+        LogKillSwitch();
+        suicide(owner);
+    }
+
+}
--- splitter/truffle.js
+++ splitter/truffle.js
@@ -1,19 +1,9 @@
 module.exports = {
   networks: {
-    development: { // This one is optional and reduces the scope for failing fast
+    development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    "net42": {
-      host: "localhost",
-      port: 8545,
-      network_id: 42
-    },
-    "ropsten": {
-      host: "localhost",
-      port: 8545,
-      network_id: 3
     }
   }
 };
